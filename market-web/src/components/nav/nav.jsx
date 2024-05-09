a2d2fce339d071563ce854397a83efeb081c621c
zxcv9675 <zxcv9675@naver.com>
2024-05-09 09:59:29 +0900

    웹사이트 구축

--- market-web/src/components/nav/nav.jsx
+++ market-web/src/components/nav/nav.jsx
@@ -6,10 +6,18 @@ export const Nav = () => {
     return (
         <div>
         <div className="navbar">
+          
           <Link className="navbarMenu" to={'/'}>MAIN</Link>
+          <Link className="navbarMenu" to={'/Desktop'}>DESKTOP</Link>
+          <Link className="navbarMenu" to={'/Monitor'}>MONITOR</Link>
           <Link className="navbarMenu" to={'/Notebook'}>NOTEBOOK</Link>
           <Link className="navbarMenu" to={'/Pad'}>PAD</Link>
           <Link className="navbarMenu" to={'/Phone'}>PHONE</Link>
+          <Link className="navbarMenu" to={'/Keyboard'}>KEYBOARD</Link>
+          <Link className="navbarMenu" to={'/Mouse'}>MOUSE</Link>
+          <Link className="navbarMenu" to={'/Game'}>GAME</Link>
+          <Link className="navbarMenu" to={'/Smartwatch'}>SMARTWATCH</Link>
+
         </div>
       </div>
 
--- market-web/src/components/products/product.jsx
+++ market-web/src/components/products/product.jsx
@@ -63,6 +63,126 @@ export const Product = () => {
           <span className={styles.unit}>원</span>
         </div>
       </div>
+
+      <div className={styles.product_item}>
+        <Link to="#">
+          <div className={styles.product_image}>
+            <img src="images/desktop.png" alt="product" />
+          </div>
+        </Link>
+        <div className={styles.store}>
+          <span>APPLE</span>
+        </div>
+
+        <div className={styles.product_name}>
+          <span>DESKTOP</span>
+        </div>
+
+        <div className={styles.product_price}>
+          <span className={styles.price}>1,835,820</span>
+          <span className={styles.unit}>원</span>
+        </div>
+      </div>
+
+      <div className={styles.product_item}>
+        <Link to="#">
+          <div className={styles.product_image}>
+            <img src="images/monitor.png" alt="product" />
+          </div>
+        </Link>
+        <div className={styles.store}>
+          <span>APPLE</span>
+        </div>
+
+        <div className={styles.product_name}>
+          <span>MONITOR</span>
+        </div>
+
+        <div className={styles.product_price}>
+          <span className={styles.price}>2,490,000</span>
+          <span className={styles.unit}>원</span>
+        </div>
+      </div>
+
+      <div className={styles.product_item}>
+        <Link to="#">
+          <div className={styles.product_image}>
+            <img src="images/keyboard.png" alt="product" />
+          </div>
+        </Link>
+        <div className={styles.store}>
+          <span>APPLE</span>
+        </div>
+
+        <div className={styles.product_name}>
+          <span>KEYBOARD</span>
+        </div>
+
+        <div className={styles.product_price}>
+          <span className={styles.price}>153,050</span>
+          <span className={styles.unit}>원</span>
+        </div>
+      </div>
+
+      <div className={styles.product_item}>
+        <Link to="#">
+          <div className={styles.product_image}>
+            <img src="images/mouse.png" alt="product" />
+          </div>
+        </Link>
+        <div className={styles.store}>
+          <span>APPLE</span>
+        </div>
+
+        <div className={styles.product_name}>
+          <span>MOUSE</span>
+        </div>
+
+        <div className={styles.product_price}>
+          <span className={styles.price}>82,770</span>
+          <span className={styles.unit}>원</span>
+        </div>
+      </div>
+
+      <div className={styles.product_item}>
+        <Link to="#">
+          <div className={styles.product_image}>
+            <img src="images/smartwatch.png" alt="product" />
+          </div>
+        </Link>
+        <div className={styles.store}>
+          <span>APPLE</span>
+        </div>
+
+        <div className={styles.product_name}>
+          <span>SMARTWATCH</span>
+        </div>
+
+        <div className={styles.product_price}>
+          <span className={styles.price}>499,900</span>
+          <span className={styles.unit}>원</span>
+        </div>
+      </div>
+
+      <div className={styles.product_item}>
+        <Link to="#">
+          <div className={styles.product_image}>
+            <img src="images/game.png" alt="product" />
+          </div>
+        </Link>
+        <div className={styles.store}>
+          <span>NINTENDO</span>
+        </div>
+
+        <div className={styles.product_name}>
+          <span>SWITCH GAME</span>
+        </div>
+
+        <div className={styles.product_price}>
+          <span className={styles.price}>63,900</span>
+          <span className={styles.unit}>원</span>
+        </div>
+      </div>
     </div>
   );
 };
