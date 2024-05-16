d21824c8ac59827cc97516ebde8845eb5c8a025b
zxcv9675 <zxcv9675@naver.com>
2024-05-16 16:32:23 +0900

    웹페이지 프로젝트 구현

--- market-web/src/components/eventBanner/eventBanner.jsx
+++ market-web/src/components/eventBanner/eventBanner.jsx
@@ -2,6 +2,8 @@ import styles from "./eventBanner.module.css";
 
 export const EventBanner = () => {
   return (
+    
+    // 배너 링크
     <a href="https://github.com/REVE97/Market_Web">
     <article
       className={styles.banner}
