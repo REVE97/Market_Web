b7564c6cf66a226b19c6fcbf1382a76136e74236
zxcv9675 <zxcv9675@naver.com>
2024-06-02 20:19:54 +0900

    실시간 가격 정보 웹페이지 프로젝트 구현

--- market-web/src/components/header/topNavigationBar/topNavigationBar.jsx
+++ market-web/src/components/header/topNavigationBar/topNavigationBar.jsx
@@ -58,20 +58,20 @@ export const TopNavigationBar = ( {cart} ) => {
           </div>
         </Link>
         
-        {/* <Link to="/">
+        <Link to="/">
           <div className={styles.mypage}>
             <img src="/images/icon-user.svg" alt="user" />
             <span>마이페이지</span>
           </div>
-        </Link> */}
+        </Link>
 
 
-        {/* <Link to="/">
+        <Link to="/">
           <div className={styles.mypage}>
             <img src="/images/icon-power.svg" alt="login" />
             <span>로그인</span>
           </div>
-        </Link> */}
+        </Link>
         
       </div>
     </header>
