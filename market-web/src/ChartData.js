8cb1efe7088785a75d43be91a4296c26b283eae2
zxcv9675 <zxcv9675@naver.com>
2024-06-12 09:00:54 +0900

    실시간 가격 정보 웹페이지 프로젝트 구현

--- market-web/src/ChartData.js
+++ market-web/src/ChartData.js
@@ -24,38 +24,20 @@
 
 // BarChart 
 // 적용할 json 파일 데이터 형식 
-axios.get(`http://3.34.188.16:8080/api/prices/summary/${product_id}`)
+// axios.get(`http://3.34.188.16:8080/api/prices/summary/${product_id}`)
 
-[
-    {
-        "product_id":5,
-        "crawl_time":"2024-06-08",
-        "high_price": 358990,
-        "low_price": 358990
-    },
-    {
-        "product_id":5,
-        "crawl_time":"2024-06-08",
-        "high_price": 358990,
-        "low_price": 358990
-    }
+// [
+//     {
+//         "product_id":5,
+//         "crawl_time":"2024-06-08",
+//         "high_price": 358990,
+//         "low_price": 358990
+//     },
+//     {
+//         "product_id":5,
+//         "crawl_time":"2024-06-08",
+//         "high_price": 358990,
+//         "low_price": 358990
+//     }
 
-]
-
-// 임의 랜덤 차트
-export const parseData = [
-   
-    ...Array.from({ length: 90 }, (_, i) => {
-        const randomStart = Math.floor(Math.random() * 5000) + 150000;
-        const randomEnd = randomStart + Math.floor(Math.random() * 20000) - 10000; 
-        const highLowMin = Math.min(randomStart, randomEnd) - 2000;
-        const highLowMax = Math.max(randomStart, randomEnd) + 2000;
-
-        return {
-            date: new Date(2024, 2, 2 + i).toISOString().split('T')[0].replace(/-/g, '.'),
-            openClose: [randomStart, randomEnd],
-            highLow: [highLowMin, highLowMax]
-        };
-    })
-
-]
+// ]
