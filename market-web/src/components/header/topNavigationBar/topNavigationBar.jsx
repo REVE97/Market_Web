4105bc44672aa9aaf601d1875068ec3dd166e233
zxcv9675 <zxcv9675@naver.com>
2024-06-15 17:51:42 +0900

    실시간 가격 정보 웹페이지 프로젝트 구현

--- market-web/src/components/header/topNavigationBar/topNavigationBar.jsx
+++ market-web/src/components/header/topNavigationBar/topNavigationBar.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import styles from "./topNavigationBar.module.css";
 
 export const TopNavigationBar = ({ cart }) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [keywords, setKeywords] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // 데이터베이스에서 키워드 목록 가져오기
+    axios.get('http://3.34.188.16:8080/api/keywords')
+      .then(response => {
+        const keywords = response.data;
+        // count를 기준으로 내림차순 정렬
+        keywords.sort((a, b) => b.count - a.count);
+        // 상위 5개만 가져오기
+        const topKeywords = keywords.slice(0, 5);
+        setKeywords(topKeywords); // 상위 5개의 키워드를 상태에 저장
+      })
+      .catch(error => {
+        console.error("Error fetching keywords:", error);
+      });
+  }, []); // 컴포넌트가 마운트될 때 한 번만 실행
 
   // 검색 엔진 함수
   const handleSearch = (e) => {
@@ -13,36 +30,29 @@ export const TopNavigationBar = ({ cart }) => {
     const trimmedSearchTerm = searchTerm.trim();
 
     if (trimmedSearchTerm) {
-      
       // 검색어를 데이터베이스에 저장
       axios.post('http://3.34.188.16:8080/api/keywords', {
-        keyword: trimmedSearchTerm,
+        keyword: trimmedSearchTerm
       })
       .then(() => {
-      // 데이터베이스에서 키워드 목록 가져오기
-        return axios.get('http://3.34.188.16:8080/api/keywords');
-      })
-      .then(response => {
-        const keywords = response.data;
-
-        // keyword를 로그로 출력
-        keywords.forEach(keyword => {
-          console.log(`Keyword: ${keyword.keyword}`);
-        });
-
         // 검색 결과 페이지로 이동
         navigate(`/search?query=${trimmedSearchTerm}`);
       })
       .catch(error => {
-        console.error("Error saving or fetching keywords:", error);
+        console.error("Error saving keyword:", error);
       });
     }
+  };
+
+  const handleKeywordClick = (keyword) => {
+    // 키워드의 이름을 검색어로 설정하고 검색 결과 페이지로 이동
+    setSearchTerm(keyword);
+    navigate(`/search?query=${keyword}`);
   };
 
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
-        
         {/* 홈 화면으로 가는 홈버튼 */}
         <Link to="/">
           <h1 className={styles.logo}>
@@ -50,7 +60,7 @@ export const TopNavigationBar = ({ cart }) => {
           </h1>
         </Link>
         
-        {/* 검색 창, 버튼 */}
+        {/* 검색 창 버튼 */}
         <div className={styles.input_wrap}>
           <form onSubmit={handleSearch}>
             <input
@@ -65,15 +75,13 @@ export const TopNavigationBar = ({ cart }) => {
           </form>
         </div>
       </div>
-   
+  
       <div className={styles.menu}>
-        
         {/* 찜목록 페이지 */}
         <Link to="/favorite">
           <div className={styles.mypage}>
             <img src="/images/icon-favorite.svg" alt="cart" />
             <span>찜목록</span>
-            
             {cart.length >= 1 ? (
               <div className={styles.new_shopping_cart}>
                 <p>{cart.length}</p>
@@ -88,16 +96,29 @@ export const TopNavigationBar = ({ cart }) => {
             <span>로그인</span>
           </div>
         </Link>
-
-
+        
         <Link to="/signup">
           <div className={styles.mypage}>
             <img src="/images/icon-power.svg" alt="login" />
             <span>회원가입</span>
           </div>
         </Link>
-        
+      </div>
+
+      {/* 키워드 버튼 */}
+      <div className={styles.keywordContainer}>
+        <span style={{ fontWeight:'bold', marginTop:'8px'}}>인기검색어 :</span>
+        {keywords.map((keywordObj) => (
+          <button
+            key={keywordObj.keyword}
+            className={styles.keywordButton}
+            onClick={() => handleKeywordClick(keywordObj.keyword)}
+          >
+            {keywordObj.keyword}
+          </button>
+        ))}
       </div>
     </header>
   );
 };
+
